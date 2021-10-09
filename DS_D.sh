8d13e4ce1512e11a21e38333ced82ca0eb55674d
efan andrian <efan.andrian05@gmail.com>
2021-10-09 17:58:45 +0700

    asd

---
+++ DS_D.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+"D:\ds_sintang\kill_DS.exe" $1 &
+git pull
+"D:\ds_sintang\DesktopService.exe" $1 &
+
---
+++ DS_E.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+"E:\ds_sintang\kill_DS.exe" $1 &
+git pull
+"E:\ds_sintang\DesktopService.exe" $1 &
+
+
